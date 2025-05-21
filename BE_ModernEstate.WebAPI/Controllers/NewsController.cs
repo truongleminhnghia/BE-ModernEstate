4991d128985f3c51bf0954780eb5cbce3d29a689
Tuan <kenjizukochannel@gmail.com>
2025-05-21 22:06:34 +0700

    cm

---
+++ BE_ModernEstate.WebAPI/Controllers/NewsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BE_ModernEstate.WebAPI.Controllers
+{
+    [Route("api/v1/news")]
+    [ApiController]
+    public class NewsController : ControllerBase
+    {
+    }
+}
