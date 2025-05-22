431f407485e214dfca5a14703b4727caf6ca97a0
truongleminhnghia <adronghia@gmail.com>
2025-05-22 16:23:34 +0700

    update api account

--- ModernEstate.Common/Models/AuthenticateResponse/AccountCurrent.cs
+++ ModernEstate.Common/Models/AuthenticateResponse/AccountCurrent.cs
@@ -5,6 +5,7 @@ namespace ModernEstate.Common.Models.AuthenticateResponse
 {
     public class AccountCurrent
     {
+        public string? Id { get; set; }
         public string? Email { get; set; }
         public string? LastName { get; set; }
         public string? FirstName { get; set; }
