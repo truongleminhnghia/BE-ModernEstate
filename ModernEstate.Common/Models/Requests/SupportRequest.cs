c1793921fe77ff4ff6c3c78704dc672480065cb8
truongleminhnghia <adronghia@gmail.com>
2025-05-22 14:28:41 +0700

    aaaa

--- ModernEstate.Common/Models/Requests/SupportRequest.cs
+++ ModernEstate.Common/Models/Requests/SupportRequest.cs
@@ -4,6 +4,8 @@ namespace ModernEstate.Common.Models.Requests
 {
     public class SupportRequest
     {
-
+        public string? SupportName { get; set; }
+        public string? SupportEmail { get; set; }
+        public string? Message { get; set; }
     }
 }
