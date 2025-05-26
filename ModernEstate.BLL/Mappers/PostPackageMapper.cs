10df987ef0e24deeffaad11f0f6f422151e1ee71
truongleminhnghia <adronghia@gmail.com>
2025-05-26 23:14:19 +0700

    add dêpndentcy

---
+++ ModernEstate.BLL/Mappers/PostPackageMapper.cs
@@ -0,0 +1,17 @@
+
+using AutoMapper;
+using ModernEstate.Common.Models.Requests;
+using ModernEstate.Common.Models.Responses;
+using ModernEstate.DAL.Entites;
+
+namespace ModernEstate.BLL.Mappers
+{
+    public class PostPackageMapper : Profile
+    {
+        public PostPackageMapper()
+        {
+            CreateMap<PostPackageReuqest, PostPackage>().ReverseMap();
+            CreateMap<PostPackage, PostPackageResponse>().ReverseMap();
+        }
+    }
+}
