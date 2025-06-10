67292f244784e3063e134f22dfc822a3bbaa5e37
truongleminhnghia <adronghia@gmail.com>
2025-06-10 12:46:36 +0700

    comkit code

---
+++ ModernEstate.Common/Enums/DisplayLanguage.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel;
+
+namespace ModernEstate.Common.Enums
+{
+    public enum DisplayLanguage
+    {
+        /// <summary>
+        /// Giao diện hiển thị bằng Tiếng Việt.  
+        /// </summary>
+        [Description("vn")]
+        Vietnamese,
+
+        /// <summary>
+        /// Giao diện hiển thị bằng Tiếng Anh.  
+        /// </summary>
+        [Description("en")]
+        English
+    }
+}
